e4f3bc2fda8f91e7fc17e87e5f49fc655c1418e7
Aldi Setiawan <aldi.setiawan@dipostar.com>
2023-07-04 15:57:34 +0700

    create api role

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from uvicorn import run
 from fastapi import FastAPI
 from app.routes.auth import router as auth
 from app.routes.permission import router as permission
+from app.routes.role import router as role
 from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="Authentication Service")
 app.include_router(router=auth, prefix="/auth", tags=["Authentication"])
 app.include_router(router=permission, prefix="/permission", tags=["Permission"])
+app.include_router(router=role, prefix="/role", tags=["Role"])
 
 
 @app.get("/")
